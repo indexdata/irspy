f8135414d1128d25c83e0f8a6810012ff2a92a7c
Mike Taylor <mike@indexdata.com>
2006-06-21 16:27:01 +0000

    New

---
+++ lib/ZOOM/IRSpy/Test/Search/Title.pm
@@ -0,0 +1,41 @@
+# $Id: Title.pm,v 1.1 2006-06-21 16:27:01 mike Exp $
+
+# See the "Main" test package for documentation
+
+package ZOOM::IRSpy::Test::Search::Title;
+
+use 5.008;
+use strict;
+use warnings;
+
+use ZOOM::IRSpy::Test;
+our @ISA;
+@ISA = qw(ZOOM::IRSpy::Test);
+
+
+sub run {
+    my $this = shift();
+    my $irspy = $this->irspy();
+    my $pod = $irspy->pod();
+
+    $pod->callback(ZOOM::Event::RECV_SEARCH, \&found);
+    $pod->search_pqf('@attr 1=4 computer');
+    my $err = $pod->wait($irspy);
+
+    return 0;
+}
+
+
+sub found {
+    my($conn, $irspy, $rs, $event) = @_;
+
+    my $rec = $irspy->record($conn);
+    my $n = $rs->size();
+    $irspy->log("irspy_test", $conn->option("host"),
+		" title search found $n record", $n==1 ? "" : "s");
+    ### Note the success or failure of the search in $rec
+    return 0;
+}
+
+
+1;
