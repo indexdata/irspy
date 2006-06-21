7c8818c661fd21c98531c5737e8dd0e5b2466d06
Mike Taylor <mike@indexdata.com>
2006-06-21 15:58:08 +0000

    Suggest more verbose logging in sample command-line.

--- irspy.pl
+++ irspy.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 
-# $Id: irspy.pl,v 1.6 2006-06-20 16:25:48 mike Exp $
+# $Id: irspy.pl,v 1.7 2006-06-21 15:58:08 mike Exp $
 #
 # Run like this:
-#	YAZ_LOG=irspy perl -I lib irspy.pl -t "bagel.indexdata.dk/gils z3950.loc.gov:7090/Voyager" localhost:1313/IR-Explain---1
+#	YAZ_LOG=irspy,irspy_test,irspy_debug perl -I lib irspy.pl -t "bagel.indexdata.dk/gils z3950.loc.gov:7090/Voyager" localhost:1313/IR-Explain---1
 
 use strict;
 use warnings;
