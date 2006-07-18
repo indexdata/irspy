240409d8b919d306703c2418bf0552a7b6f47bfd
Mike Taylor <mike@indexdata.com>
2006-07-18 10:40:13 +0000

    Note failure as well as success.

--- lib/ZOOM/IRSpy/Test/Ping.pm
+++ lib/ZOOM/IRSpy/Test/Ping.pm
@@ -1,4 +1,4 @@
-# $Id: Ping.pm,v 1.5 2006-07-11 14:16:06 mike Exp $
+# $Id: Ping.pm,v 1.6 2006-07-18 10:40:13 mike Exp $
 
 # See the "Main" test package for documentation
 
@@ -19,17 +19,23 @@ sub run {
     my $pod = $irspy->pod();
 
     $pod->callback(ZOOM::Event::CONNECT, \&connected);
+    $pod->callback("exception", \&not_connected);
     my $err = $pod->wait($irspy);
 
     return 0;
 }
 
 
-sub connected {
-    my($conn, $irspy, $rs, $event) = @_;
+sub connected { maybe_connected(@_, 1) }
+sub not_connected { maybe_connected(@_, 0) }
+
+sub maybe_connected {
+    my($conn, $irspy, $rs, $event, $ok) = @_;
 
     my $rec = $irspy->record($conn);
-    $irspy->log("irspy_test", $conn->option("host"), " connected");
+    $irspy->log("irspy_test", $conn->option("host"),
+		($ok ? "" : " not"), " connected");
+    $rec->failed(1) if !$ok;
     ### At this point we should note the successful connection in $rec
     return 0;
 }
