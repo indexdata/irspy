6a4b8fd3ac0d69d0fc93971c40ff1eee6c68f689
Mike Taylor <mike@indexdata.com>
2006-09-22 15:27:17 +0000

    USe new API for targets(). This requires a different command-line.

--- bin/irspy.pl
+++ bin/irspy.pl
@@ -1,34 +1,23 @@
 #!/usr/bin/perl -w
 
-# $Id: irspy.pl,v 1.6 2006-09-22 15:23:51 mike Exp $
+# $Id: irspy.pl,v 1.7 2006-09-22 15:27:17 mike Exp $
 #
 # Run like this:
 #	YAZ_LOG=irspy,irspy_test,irspy_debug perl -I ../lib irspy.pl -t "bagel.indexdata.dk/gils z3950.loc.gov:7090/Voyager" localhost:3313/IR-Explain---1
 
 use strict;
 use warnings;
-use Getopt::Std;
 use ZOOM::IRSpy;
 
-my %opts;
-if (!getopts('t:a', \%opts) || @ARGV != 1) {
-    print STDERR qq[Usage: $0 [options] <IRSpy-database>
-	-t <t1 t2 ...>	Space-separated list of targets to check
-	-a		Check all targets registered in database
-];
+my($dbname, @targets) = @ARGV;
+if (!defined $dbname) {
+    print STDERR "Usage $0: <IRSpy-database> [<target> ...]\n";
+    print STDERR "If no targets are specified, all targets in DB are tested\n";
     exit 1;
 }
 
-my $dbname = $ARGV[0];
-my $targetList = $opts{t};
-if (!defined $targetList && !$opts{a}) {
-    print STDERR "$0: neither -t nor -a specified\n";
-    exit 2;
-}
-
 my $spy = new ZOOM::IRSpy($dbname, "admin", "fruitbat");
-$spy->targets(grep { $_ ne "" } split /\s+/, $targetList)
-    if defined $targetList;
+$spy->targets(@targets) if @targets;
 $spy->initialise();
 my $res = $spy->check();
 if ($res == 0) {
