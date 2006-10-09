df9edbc287f11442af7ec46a73303bf4a5a6e4ed
Mike Taylor <mike@indexdata.com>
2006-10-09 07:29:43 +0000

    Add explicit "-t test" to sample command-line

--- bin/irspy.pl
+++ bin/irspy.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 
-# $Id: irspy.pl,v 1.10 2006-10-06 11:25:11 mike Exp $
+# $Id: irspy.pl,v 1.11 2006-10-09 07:29:43 mike Exp $
 #
 # Run like this:
-#	YAZ_LOG=irspy,irspy_test,irspy_debug,irspy_event perl -I ../lib irspy.pl localhost:3313/IR-Explain---1 bagel.indexdata.dk/gils z3950.loc.gov:7090/Voyager bagel.indexdata.dk:210/marc 
+#	YAZ_LOG=irspy,irspy_test,irspy_debug,irspy_event perl -I ../lib irspy.pl -t Main localhost:3313/IR-Explain---1 bagel.indexdata.dk/gils z3950.loc.gov:7090/Voyager bagel.indexdata.dk:210/marc
 
 use strict;
 use warnings;
