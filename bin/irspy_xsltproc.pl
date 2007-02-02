e04a51eb8137d29b22ff514f4c226192fbd530b8
Anders S. Mortensen <sondberg@indexdata.dk>
2007-02-02 12:40:51 +0000

    Applied Mike's nice -d option thing.

--- bin/irspy_xsltproc.pl
+++ bin/irspy_xsltproc.pl
@@ -1,11 +1,16 @@
 #!/usr/bin/perl -w
 
-# $Id: irspy_xsltproc.pl,v 1.1 2007-02-02 12:38:48 sondberg Exp $
+# $Id: irspy_xsltproc.pl,v 1.2 2007-02-02 12:40:51 sondberg Exp $
 
 use strict;
 use warnings;
 use lib '../lib';
 use ZOOM::IRSpy;
+
+if (@ARGV && $ARGV[0] eq "-d") {
+    shift;
+    XML::LibXSLT->debug_callback(\&xslt_debug);
+}
 
 my $dbname = 'localhost:8018/IR-Explain---1';
 my $spy = new ZOOM::IRSpy($dbname, "admin", "fruitbat");
