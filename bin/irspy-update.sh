0dfe0264eb16b21e1e7d8ca050b2df8fe67648eb
Wolfram Schneider <wosch@indexdata.dk>
2010-04-19 19:58:01 +0000

    date(1) typo

--- bin/irspy-update.sh
+++ bin/irspy-update.sh
@@ -11,7 +11,7 @@ logdir=../tmp
 
 for i in 0 1 2 3 4 5 6
 do
-   logfile=$logdir/irspy-mod-$i.log.`date '%w'`
+   logfile=$logdir/irspy-mod-$i.log.`date '+%w'`
    YAZ_LOG=irspy,irspy_test nice -10 time perl -I../lib irspy.pl -n 50 -d -M 3500 -a -t Main -m 7,$i localhost:8018/IR-Explain---1 > $logfile 2>&1
    gzip -f $logfile
 done
