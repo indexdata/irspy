4209539f05fd9766a686e84f0a85a479aba11952
John Malconian <malc@indexdata.com>
2022-10-25 14:37:35 +0000

    slightly modified irspy-update.sh script that will run in local Docker environment.

---
+++ bin/irspy-update-docker.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# Copyright (c) 2010 Index Data, http://www.indexdata.com
+#
+# irspy-update.sh - wrapper for irspy.pl
+#
+# run irspy with a smaller set of records in a loop to avoid out-of-memory
+#
+# for a fast update, run this:
+# 	$ env irspy_test=Quick ./irspy-update.sh
+
+home=/home/indexdata
+cd $home/irspy/bin || exit 2
+logdir=../log
+lockfile=$logdir/irspy-update.lock
+statusfile=$logdir/irspy-last-update.log
+
+# run a full update by default, use Quick for a fast update
+: ${irspy_test=Main}
+
+mkdir -p $logdir || exit2
+
+if [ -f $lockfile ]; then
+    pid=`cat $lockfile`
+    if kill -0 $pid 2>/dev/null; then
+	echo "This script is already running with pid: $pid"
+	exit 1
+    fi
+fi
+echo $$ > $lockfile || exit 2
+
+weekday=`date '+%w'`
+for i in 0 1 2 3 4 5 6 7 8
+do
+   logfile=$logdir/irspy-mod-$i.log.$weekday
+   IRSPY_COUNTER_FILE=/home/indexdata/irspy/zebra/db/counter YAZ_LOG=irspy,irspy_test,irspy_task nice -10 time perl -I../lib irspy.pl -n 50 -d -M 3500 -f'cql.allRecords=1 not zeerex.disabled = 1' -t $irspy_test -r ../etc/dallas.rules -m 9,$i irspy-zebra:8018/IR-Explain---1 > $logfile 2>&1
+
+   sleep 1 # catch ctr-c before compressing the log
+   gzip -f $logfile
+done
+
+rm -f $lockfile
+date > $statusfile
+
